1828b5e09692976ab655ae324cf61d1389cbcfaa
Erika Koskinen <bgx093@myy.haaga-helia.fi>
2023-08-27 21:04:22 +0300

    Tehtävä 2 tehty

--- App.js
+++ App.js
@@ -1,10 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, Alert } from 'react-native';
 
 export default function App() {
+
+  const [guessedNumber, setGuessedNumber] = useState("");
+  const [text, setText] = useState("Guess a number between 1-100");
+  const [rightNumber, setRightNumber] = useState(Math.floor(Math.random() * 100) + 1);
+  const [timesGuessed, setTimesGuessed] = useState(0);
+
+  const play = () => {
+
+    const guessed = parseInt(guessedNumber);
+
+    if (guessed < rightNumber) {
+      setText("Your guess " + guessed + " is too low");
+      setTimesGuessed(timesGuessed + 1);
+    } else if (guessed > rightNumber) {
+      setText("Your guess " + guessed + " is too high");
+      setTimesGuessed(timesGuessed + 1);
+    } else if (guessed === rightNumber) {
+      Alert.alert("You guessed the number in " + timesGuessed + " guesses");
+      setText("Guess a number between 1-100");
+      setGuessedNumber("");
+      setRightNumber(Math.floor(Math.random() * 100) + 1);
+      setTimesGuessed(0);
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{text}</Text>
+      <TextInput style={styles.input} inputMode='numeric' onChangeText={guessedNumber => setGuessedNumber(guessedNumber)} value={guessedNumber} />
+      <Button onPress={play} title="Make a guess" />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +45,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    width: 30,
+    borderColor: 'black',
+    borderWidth: 1,
+    marginTop: 10,
+    marginBottom: 5
+  }
 });
